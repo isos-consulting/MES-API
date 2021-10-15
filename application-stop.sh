7358d9efe43bc8846bd11b660da4b51de7c0f315
cyj <yj.cho@kisos.net>
2021-10-15 15:19:48 +0900

    test

--- application-stop.sh
+++ application-stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-echo "/root/node_modules/.bin/pm2 start --name iso-server yarn -- start"
+echo "/root/node_modules/.bin/pm2 start --name iso-server yarn -- start1"
 /root/node_modules/.bin/pm2 kill
