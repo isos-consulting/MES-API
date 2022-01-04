d03a32d4bf44be6c4f825bbb1c5489c7d04893bf
won <jh.won@kisos.net>
2022-01-04 19:52:18 +0900

    done is-67

--- src/services/adm/daily-insp-cycle.service.ts
+++ src/services/adm/daily-insp-cycle.service.ts
@@ -1,7 +1,5 @@
 import { Transaction } from "sequelize/types";
 import AdmDailyInspCycleRepo from "../../repositories/adm/daily-insp-cycle.repository";
-import { successState } from "../../states/common.state";
-import createApiResult from "../../utils/createApiResult";
 
 class AdmDailyInspCycleService {
   tenant: string;
@@ -15,57 +13,33 @@ class AdmDailyInspCycleService {
   }
 
   public create = async (datas: any[], uid: number, tran: Transaction) => {
-    try {
-      const result = await this.repo.create(datas, uid, tran);
-      return createApiResult(result, 201, '데이터 생성 성공', this.stateTag, successState.CREATE);
-    } catch (error) {
-      throw error;
-    }
+    try { return await this.repo.create(datas, uid, tran); }
+		catch (error) { throw error; }
   }
 
   public read = async (params: any) => {
-    try {
-      const result = await this.repo.read(params);
-      return createApiResult(result, 200, '데이터 조회 성공', this.stateTag, successState.READ);
-    } catch (error) {
-      throw error;
-    }
+    try { return await this.repo.read(params); }
+		catch (error) { throw error; }
   };
   
   public readByUuid = async (uuid: string) => {
-    try {
-      const result = await this.repo.readByUuid(uuid);
-      return createApiResult(result, 200, '데이터 조회 성공', this.stateTag, successState.READ);
-    } catch (error) {
-      throw error;
-    }
+    try { return await this.repo.readByUuid(uuid); }
+		catch (error) { throw error; }
   };
 
   public update = async (datas: any[], uid: number, tran: Transaction) => {
-    try {
-      const result = await this.repo.update(datas, uid, tran);
-      return createApiResult(result, 200, '데이터 수정 성공', this.stateTag, successState.UPDATE);
-    } catch (error) {
-      throw error;
-    }
+    try { return await this.repo.update(datas, uid, tran); }
+		catch (error) { throw error; }
   }
 
   public patch = async (datas: any[], uid: number, tran: Transaction) => {
-    try {
-      const result = await this.repo.patch(datas, uid, tran);
-      return createApiResult(result, 200, '데이터 수정 성공', this.stateTag, successState.PATCH);
-    } catch (error) {
-      throw error;
-    }
+    try { return await this.repo.patch(datas, uid, tran); }
+		catch (error) { throw error; }
   }
 
   public delete = async (datas: any[], uid: number, tran: Transaction) => {
-    try {
-      const result = await this.repo.delete(datas, uid, tran);
-      return createApiResult(result, 200, '데이터 삭제 성공', this.stateTag, successState.DELETE);
-    } catch (error) {
-      throw error;
-    }
+    try { return await this.repo.delete(datas, uid, tran); }
+		catch (error) { throw error; }
   }
 }
 
