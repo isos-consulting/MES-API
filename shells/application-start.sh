bb8a9daf1fbfaae9a1fe9f98fa9b5b68cc4dd23a
dkc <kc.do@kisos.net>
2022-01-24 16:19:55 +0900

    update development env file in application-start.sh

--- shells/application-start.sh
+++ shells/application-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
-aws s3 cp s3://s3-isos-environment/iso.env /var/iso-server/.env
+aws s3 cp s3://s3-isos-environment/iso.dev.env /var/iso-server/.env
 cd /var/iso-server
 /root/node_modules/.bin/pm2 start --name iso-server yarn -- start
