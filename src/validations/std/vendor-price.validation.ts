e16cd13424269ab917462c38127b4b65b2ae33b7
dkc <kc.do@kisos.net>
2022-03-15 13:41:22 +0900

    done is-185

--- src/validations/std/vendor-price.validation.ts
+++ src/validations/std/vendor-price.validation.ts
@@ -6,37 +6,37 @@ const stateTag = 'stdVendorPrice';
 
 const stdVendorPriceValidation = {
 	upsertBulkDatasFromExcel: [
-		query('uuid', '협력사 단가UUID').optional({ nullable: true })
+		body('*.uuid', '협력사 단가UUID').optional({ nullable: true })
       .isUUID().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'uuid', '협력사 단가UUID')),
-		query('partner_cd', '거래처코드')
+		body('*.partner_cd', '거래처코드')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'partner_cd', '거래처코드'))
       .isString().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'partner_cd', '거래처코드')),
-		query('prod_no', '품번')
+		body('*.prod_no', '품번')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'prod_no', '품번'))
       .isString().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'prod_no', '품번')),
-		query('rev', '리비전')
+		body('*.rev', '리비전')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'rev', '리비전'))
       .isString().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'rev', '리비전')),
-		query('unit_cd', '단위코드')
+		body('*.unit_cd', '단위코드')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'unit_cd', '단위코드'))
       .isString().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'unit_cd', '단위코드')),
-		query('price', '단가')
+		body('*.price', '단가')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'price', '단가'))
       .isNumeric().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'price', '단가')),
-		query('money_unit_cd', '화폐단위코드')
+		body('*.money_unit_cd', '화폐단위코드')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'money_unit_cd', '화폐단위코드'))
       .isString().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'money_unit_cd', '화폐단위코드')),
-		query('price_type_cd', '단가유형코드')
+		body('*.price_type_cd', '단가유형코드')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'price_type_cd', '단가유형코드'))
       .isString().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'price_type_cd', '단가유형코드')),
-		query('start_date', '단가적용일자')
+		body('*.start_date', '단가적용일자')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'start_date', '단가적용일자'))
       .isDate().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'start_date', '단가적용일자')),
-		query('retroactive_price', '소급단가').optional({ nullable: true })
+		body('*.retroactive_price', '소급단가').optional({ nullable: true })
       .isNumeric().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'retroactive_price', '소급단가')),
-		query('division', '소급단가').optional({ nullable: true })
+		body('*.division', '소급단가').optional({ nullable: true })
       .isNumeric().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'division', '소급단가')),
-		query('remark', '비고').optional({ nullable: true })
+		body('*.remark', '비고').optional({ nullable: true })
       .isString().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'remark', '비고')),
 	],
 	read: [
@@ -53,74 +53,74 @@ const stdVendorPriceValidation = {
       .isUUID().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'uuid', '협력사 단가UUID'))
   ],
   create: [
-		query('partner_uuid', '거래처UUID')
+		body('*.partner_uuid', '거래처UUID')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'partner_uuid', '거래처UUID'))
       .isUUID().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'partner_uuid', '거래처UUID')),
-		query('prod_uuid', '품목UUID')
+		body('*.prod_uuid', '품목UUID')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'prod_uuid', '품목UUID'))
       .isUUID().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'prod_uuid', '품목UUID')),
-		query('unit_uuid', '단위UUID')
+		body('*.unit_uuid', '단위UUID')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'unit_uuid', '단위UUID'))
       .isUUID().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'unit_uuid', '단위UUID')),
-		query('price', '단가')
+		body('*.price', '단가')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'price', '단가'))
       .isNumeric().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'price', '단가')),
-		query('money_unit_uuid', '화폐단위UUID')
+		body('*.money_unit_uuid', '화폐단위UUID')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'money_unit_uuid', '화폐단위UUID'))
       .isUUID().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'money_unit_uuid', '화폐단위UUID')),
-		query('price_type_uuid', '단가유형UUID')
+		body('*.price_type_uuid', '단가유형UUID')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'price_type_uuid', '단가유형UUID'))
       .isUUID().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'price_type_uuid', '단가유형UUID')),
-		query('start_date', '단가적용일자')
+		body('*.start_date', '단가적용일자')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'start_date', '단가적용일자'))
       .isDate().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'start_date', '단가적용일자')),
-		query('retroactive_price', '소급단가').optional({ nullable: true })
+		body('*.retroactive_price', '소급단가').optional({ nullable: true })
       .isNumeric().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'retroactive_price', '소급단가')),
-		query('division', '배분율').optional({ nullable: true })
+		body('*.division', '배분율').optional({ nullable: true })
       .isNumeric().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'division', '배분율')),
-		query('remark', '비고').optional({ nullable: true })
+		body('*.remark', '비고').optional({ nullable: true })
       .isString().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'remark', '비고')),
 	],
   update: [
-		query('uuid', '협력사 단가UUID')
+		body('*.uuid', '협력사 단가UUID')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'uuid', '협력사 단가UUID'))
 			.isString().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'uuid', '협력사 단가UUID')),
-		query('price', '단가')
+		body('*.price', '단가')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'price', '단가'))
       .isNumeric().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'price', '단가')),
-		query('money_unit_uuid', '화폐단위UUID')
+		body('*.money_unit_uuid', '화폐단위UUID')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'money_unit_uuid', '화폐단위UUID'))
       .isUUID().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'money_unit_uuid', '화폐단위UUID')),
-		query('price_type_uuid', '단가유형UUID')
+		body('*.price_type_uuid', '단가유형UUID')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'price_type_uuid', '단가유형UUID'))
       .isUUID().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'price_type_uuid', '단가유형UUID')),
-		query('start_date', '단가적용일자')
+		body('*.start_date', '단가적용일자')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'start_date', '단가적용일자'))
       .isDate().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'start_date', '단가적용일자')),
-		query('retroactive_price', '소급단가').optional({ nullable: true })
+		body('*.retroactive_price', '소급단가').optional({ nullable: true })
       .isNumeric().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'retroactive_price', '소급단가')),
-		query('division', '배분율').optional({ nullable: true })
+		body('*.division', '배분율').optional({ nullable: true })
       .isNumeric().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'division', '배분율')),
-		query('remark', '비고').optional({ nullable: true })
+		body('*.remark', '비고').optional({ nullable: true })
       .isString().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'remark', '비고')),
   ],
   patch: [
-    query('uuid', '협력사 단가UUID')
+    body('*.uuid', '협력사 단가UUID')
 			.notEmpty().withMessage(value => createValidationError(value, stateTag, errorState.NO_INPUT_REQUIRED_PARAM, 400, 'uuid', '협력사 단가UUID'))
 			.isString().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'uuid', '협력사 단가UUID')),
-		query('price', '단가').optional({ nullable: true })
+		body('*.price', '단가').optional({ nullable: true })
       .isNumeric().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'price', '단가')),
-		query('money_unit_uuid', '화폐단위UUID').optional({ nullable: true })
+		body('*.money_unit_uuid', '화폐단위UUID').optional({ nullable: true })
       .isUUID().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'money_unit_uuid', '화폐단위UUID')),
-		query('price_type_uuid', '단가유형UUID').optional({ nullable: true })
+		body('*.price_type_uuid', '단가유형UUID').optional({ nullable: true })
       .isUUID().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'price_type_uuid', '단가유형UUID')),
-		query('start_date', '단가적용일자').optional({ nullable: true })
+		body('*.start_date', '단가적용일자').optional({ nullable: true })
       .isDate().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'start_date', '단가적용일자')),
-		query('retroactive_price', '소급단가').optional({ nullable: true })
+		body('*.retroactive_price', '소급단가').optional({ nullable: true })
       .isNumeric().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'retroactive_price', '소급단가')),
-		query('division', '배분율').optional({ nullable: true })
+		body('*.division', '배분율').optional({ nullable: true })
       .isNumeric().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'division', '배분율')),
-		query('remark', '비고').optional({ nullable: true })
+		body('*.remark', '비고').optional({ nullable: true })
       .isString().withMessage(value => createValidationError(value, stateTag, errorState.INVALID_READ_PARAM, 400, 'remark', '비고')),
   ],
   delete: [
