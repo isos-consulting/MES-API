6c723bd44e3f1258ab4ce8bf50f46d415888491f
won <jh.won@kisos.net>
2022-04-01 11:24:44 +0900

    done feature/is-242

---
+++ src/services/sal/order-detail.service.ts
@@ -0,0 +1,149 @@
+import { Transaction } from "sequelize/types";
+import SalOrderDetailRepo from "../../repositories/sal/order-detail.repository";
+import SalOrderRepo from "../../repositories/sal/Order.repository";
+import StdFactoryRepo from "../../repositories/std/factory.repository";
+import StdMoneyUnitRepo from "../../repositories/std/money-unit.repository";
+import StdProdRepo from "../../repositories/std/prod.repository";
+import getFkIdByUuid, { getFkIdInfo } from "../../utils/getFkIdByUuid";
+
+class SalOrderDetailService {
+  tenant: string;
+  stateTag: string;
+  repo: SalOrderDetailRepo;
+  fkIdInfos: getFkIdInfo[];
+
+  constructor(tenant: string) {
+    this.tenant = tenant;
+    this.stateTag = 'salOrderDetail';
+    this.repo = new SalOrderDetailRepo(tenant);
+
+    this.fkIdInfos = [
+      {
+        key: 'uuid',
+        TRepo: SalOrderDetailRepo,
+        idName: 'order_detail_id',
+        uuidName: 'uuid'
+      },
+      {
+        key: 'orderDetail',
+        TRepo: SalOrderDetailRepo,
+        idName: 'order_detail_id',
+        uuidName: 'order_detail_uuid'
+      },
+      {
+        key: 'order',
+        TRepo: SalOrderRepo,
+        idName: 'order_id',
+        uuidName: 'order_uuid'
+      },
+      {
+        key: 'factory',
+        TRepo: StdFactoryRepo,
+        idName: 'factory_id',
+        uuidName: 'factory_uuid'
+      },
+      {
+        key: 'prod',
+        TRepo: StdProdRepo,
+        idName: 'prod_id',
+        uuidName: 'prod_uuid'
+      },
+      {
+        key: 'moneyUnit',
+        TRepo: StdMoneyUnitRepo,
+        idName: 'money_unit_id',
+        uuidName: 'money_unit_uuid'
+      },
+    ];
+  }
+
+  public convertFk = async (datas: any) => {
+    // ✅ CUD 연산이 실행되기 전 Fk Table 의 uuid 로 id 를 검색하여 request body 에 삽입하기 위하여 정보 Setting
+    return await getFkIdByUuid(this.tenant, datas, this.fkIdInfos);
+  }
+
+  public create = async (datas: any[], uid: number, tran: Transaction) => {
+    try { return await this.repo.create(datas, uid, tran); } 
+    catch (error) { throw error; }
+  }
+
+  public read = async (params: any) => {
+    try { return await this.repo.read(params); } 
+    catch (error) { throw error; }
+  };
+  
+  public readByUuid = async (uuid: string) => {
+    try { return await this.repo.readByUuid(uuid); } 
+    catch (error) { throw error; }
+  };
+
+  public readRawByUuid = async (uuid: string) => {
+    try { return await this.repo.readRawByUuid(uuid); } 
+    catch (error) { throw error; }
+  };
+
+  public readRawById = async (id: number) => {
+    try { return await this.repo.readRawById(id); } 
+    catch (error) { throw error; }
+  };
+
+  public update = async (datas: any[], uid: number, tran: Transaction) => {
+    try { return await this.repo.update(datas, uid, tran); } 
+    catch (error) { throw error; }
+  }
+
+  public patch = async (datas: any[], uid: number, tran: Transaction) => {
+    try { return await this.repo.patch(datas, uid, tran); } 
+    catch (error) { throw error; }
+  }
+
+  public delete = async (datas: any[], uid: number, tran: Transaction) => {
+    try { return await this.repo.delete(datas, uid, tran); } 
+    catch (error) { throw error; }
+  }
+
+	public updateComplete = async (datas: any[], uid: number, tran: Transaction) => {
+    try { return await this.repo.updateComplete(datas, uid, tran); } 
+    catch (error) { throw error; }
+  }
+
+  /**
+   * 입력한 전표에 해당하는 상세전표의 개수 조회
+   * @param id 전표의 ID
+   * @param tran DB Transaction
+   * @returns 상세전표의 개수
+   */
+  public getCountInHeader = async (id: number, tran?: Transaction) => {
+    try { return await this.repo.getCount(id, tran); } 
+    catch (error) { throw error; }
+  }
+
+  /**
+   * 입력한 전표에 해당하는 상세전표의 Max Sequence 조회
+   * @param id 전표의 ID
+   * @param tran DB Transaction
+   * @returns Sequence
+   */
+  public getMaxSeq = async (id: number, tran?: Transaction) => {
+    try { return await this.repo.getMaxSeq(id, tran); } 
+    catch (error) { throw error; }
+  }
+
+  /**
+   * 자재입하상세 데이터의 입하수량 * 단가 * 환율을 합계금액(total_price)로 입력하여 수정
+   * @param datas 자재입하상세 데이터
+   * @param uid 입력 사용자ID
+   * @param tran DB Transaction
+   * @returns total_price가 추가 된 자재입하상세 데이터
+   */
+  public updateTotalPrice = async (datas: any[], uid: number, tran?: Transaction) => {
+    datas = datas.map((data: any) => {
+      data.total_price = data.qty * data.price * data.exchange; 
+      return data;
+    });
+
+    return await this.repo.patch(datas, uid, tran);
+  }
+}
+
+export default SalOrderDetailService;
---
+++ src/services/sal/order.service.ts
@@ -0,0 +1,116 @@
+import { Transaction } from "sequelize/types";
+import SalOrderDetailRepo from "../../repositories/sal/order-detail.repository";
+import SalOrderRepo from "../../repositories/sal/order.repository";
+import StdFactoryRepo from '../../repositories/std/factory.repository';
+import StdPartnerRepo from '../../repositories/std/partner.repository';
+import getFkIdByUuid, { getFkIdInfo } from "../../utils/getFkIdByUuid";
+
+class SalOrderService {
+  tenant: string;
+  stateTag: string;
+  repo: SalOrderRepo;
+  detailRepo: SalOrderDetailRepo;
+  fkIdInfos: getFkIdInfo[];
+
+  constructor(tenant: string) {
+    this.tenant = tenant;
+    this.stateTag = 'salOrder';
+    this.repo = new SalOrderRepo(tenant);
+    this.detailRepo = new SalOrderDetailRepo(tenant);
+
+    this.fkIdInfos = [
+      {
+        key: 'uuid',
+        TRepo: SalOrderRepo,
+        idName: 'order_id',
+        uuidName: 'uuid'
+      },
+      {
+        key: 'order',
+        TRepo: SalOrderRepo,
+        idName: 'order_id',
+        uuidName: 'order_uuid'
+      },
+      {
+        key: 'factory',
+        TRepo: StdFactoryRepo,
+        idName: 'factory_id',
+        uuidName: 'factory_uuid'
+      },
+      {
+        key: 'partner',
+        TRepo: StdPartnerRepo,
+        idName: 'partner_id',
+        uuidName: 'partner_uuid'
+      },
+    ];
+  }
+
+  public convertFk = async (datas: any) => {
+    // ✅ CUD 연산이 실행되기 전 Fk Table 의 uuid 로 id 를 검색하여 request body 에 삽입하기 위하여 정보 Setting
+    return await getFkIdByUuid(this.tenant, datas, this.fkIdInfos);
+  }
+
+  public create = async (datas: any[], uid: number, tran: Transaction) => {
+    try { return await this.repo.create(datas, uid, tran); } 
+    catch (error) { throw error; }
+  }
+
+  public read = async (params: any) => {
+    try { return await this.repo.read(params); } 
+    catch (error) { throw error; }
+  };
+  
+  public readByUuid = async (uuid: string) => {
+    try { return await this.repo.readByUuid(uuid); } 
+    catch (error) { throw error; }
+  };
+
+  public readReport = async (params: any) => {
+    try { return await this.repo.readReport(params); } 
+    catch (error) { throw error; }
+  };
+
+  public update = async (datas: any[], uid: number, tran: Transaction) => {
+    try { return await this.repo.update(datas, uid, tran); } 
+    catch (error) { throw error; }
+  }
+
+  public patch = async (datas: any[], uid: number, tran: Transaction) => {
+    try { return await this.repo.patch(datas, uid, tran); } 
+    catch (error) { throw error; }
+  }
+
+  public delete = async (datas: any[], uid: number, tran: Transaction) => {
+    try { return await this.repo.delete(datas, uid, tran); } 
+    catch (error) { throw error; }
+  }
+
+  // 📒 Fn[updateTotal]: 전표 합계 금액, 수량 계산
+  /**
+   * 전표 합계 금액, 수량 계산
+   * @param _id 전표 Id
+   * @param _uuid 전표 Uuid
+   * @param _uid 데이터 수정자 Uid
+   * @param _transaction Transaction
+   * @returns 합계 금액, 수량이 계산 된 전표 결과
+   */
+  updateTotal = async (id: number, uuid: string, uid: number, tran?: Transaction) => {
+    const getTotals = await this.detailRepo.getTotals(id, tran);
+    const totalQty = getTotals?.totalQty;
+    const totalPrice = getTotals?.totalPrice;
+
+    const result = await this.repo.patch(
+      [{ 
+        total_qty: totalQty,
+        total_price: totalPrice,
+        uuid: uuid,
+      }], 
+      uid, tran
+    );
+
+    return result;
+  }
+}
+
+export default SalOrderService;
