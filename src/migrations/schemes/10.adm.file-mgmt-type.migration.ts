7276abf848012c09bfa5e08a34219ab7d9e4833b
won <jh.won@kisos.net>
2022-05-27 09:37:49 +0900

    file-mgmt-type, file-mgmt-detail-type migration chaged

--- src/migrations/schemes/10.adm.file-mgmt-type.migration.ts
+++ src/migrations/schemes/10.adm.file-mgmt-type.migration.ts
@@ -13,7 +13,7 @@ const seedDatas: IAdmFileMgmtType[] = [
 		uuid : "cfcfb6e9-945a-427b-a383-332c748009d5"
 	},
 	{
-		file_mgmt_type_id : 5,
+		file_mgmt_type_id : 2,
 		file_mgmt_type_cd : "FIL_STD_PROD",
 		file_mgmt_type_nm : "품목관리 첨부",
 		sortby : 1,
@@ -22,7 +22,7 @@ const seedDatas: IAdmFileMgmtType[] = [
 		uuid : "d3d25d0d-73cc-46bd-a679-b6952631f992"
 	},
 	{
-		file_mgmt_type_id : 6,
+		file_mgmt_type_id : 3,
 		file_mgmt_type_cd : "FIL_STD_EMP",
 		file_mgmt_type_nm : "사원관리 첨부",
 		sortby : 2,
@@ -31,7 +31,7 @@ const seedDatas: IAdmFileMgmtType[] = [
 		uuid : "c9294cad-6560-4b54-9ece-65000123cec0"
 	},
 	{
-		file_mgmt_type_id : 7,
+		file_mgmt_type_id : 4,
 		file_mgmt_type_cd : "FIL_STD_EQUIP",
 		file_mgmt_type_nm : "설비관리 첨부",
 		sortby : 3,
@@ -40,7 +40,7 @@ const seedDatas: IAdmFileMgmtType[] = [
 		uuid : "2dd158bf-8a3c-4871-8f6d-e2155ac23a22"
 	},
 	{
-		file_mgmt_type_id : 8,
+		file_mgmt_type_id : 5,
 		file_mgmt_type_cd : "FIL_QMS_INSP",
 		file_mgmt_type_nm : "검사기준서 첨부",
 		sortby : 4,
