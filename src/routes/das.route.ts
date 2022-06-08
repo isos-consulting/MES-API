3c61f7d94c3ad9a79164cc3938317df5cec02b7b
dkc <kc.do@kisos.net>
2022-06-08 17:19:26 +0900

    done is-281

--- src/routes/das.route.ts
+++ src/routes/das.route.ts
@@ -1,5 +1,4 @@
 import * as express from 'express';
-import Dashboard2Ctl from '../controllers/das/dashboard2.controller';
 import DashboardCtl from '../controllers/das/dashboard.controller';
 import validationCallback from '../utils/validationCallback';
 import dashboardValidation from '../validations/das/dashboard.validation';
@@ -10,14 +9,6 @@ const router = express.Router();
 const dashboard = new DashboardCtl();
 router.route('/overall-status').get(dashboardValidation.readOverallStatus, validationCallback, dashboard.readOverallStatus);
 router.route('/realtime-status').get(dashboardValidation.readRealtimeStatus, validationCallback, dashboard.readRealtimeStatus);
-
-
-const dashboard2 = new Dashboard2Ctl();
-router.route('/work-compared-order').get(dashboard2.readWorkComparedOrder);
-router.route('/passed-insp-result').get(dashboard2.readPassedInspResult);
-router.route('/delayed-sal-order').get(dashboard2.readDelayedSalOrder);
-router.route('/operating-rate').get(dashboard2.readOperatingRate);
-router.route('/delivered-in-week').get(dashboard2.readDeliveredInWeek);
 //#endregion
 
 export default router;
